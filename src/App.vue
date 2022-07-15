c0f9837c4b6d97e72343ba7334afe4ce8ac614e5
hehuiting <hehuiting@hehuitingdeMacBook-Pro.local>
2022-07-15 17:29:30 +0800

    版本初始化

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,99 @@
+<!--
+ * @Author: heht
+ * @Date: 2022-03-31 21:21:25
+ * @LastEditors: heht
+ * @LastEditTime: 2022-06-28 15:12:40
+ * @Description: 
+-->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- <div id="map" class="map"></div> -->
     <router-view />
   </div>
 </template>
+
+<script>
+import "ol/ol.css";
+import Map from "ol/Map";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
+import proj4 from "proj4";
+import { TileWMS } from "ol/source";
+import { get as getProjection } from "ol/proj";
+import { register } from "ol/proj/proj4";
+export default {
+  mounted() {
+    // this.mapInit();
+  },
+
+  methods: {
+    mapInit() {
+      // 1、注册坐标系
+      proj4.defs(
+        "EPSG:21781",
+        "+proj=somerc +lat_0=46.95240555555556 " +
+          "+lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel " +
+          "+towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs"
+      );
+
+      proj4.defs(
+        "EPSG:4522",
+        "+proj=tmerc +lat_0=0 +lon_0=102 +k=1 +x_0=34500000 +y_0=0 +ellps=GRS80 +units=m +no_defs"
+      );
+      register(proj4);
+
+      // 2、定义该坐标系视图范围
+      let proj21781 = getProjection("EPSG:21781");
+      proj21781.setExtent([485071.54, 75346.36, 828515.78, 299941.84]);
+
+      const proj4522 = getProjection("EPSG:4522");
+      proj4522.setExtent([
+        34599537.78543863, 3313030.6622967534, 34603202.65556276,
+        3316518.9696675064,
+      ]);
+
+      let layers = {};
+
+      //  3、底图坐标4326
+      layers["wms4522"] = new TileLayer({
+        source: new TileWMS({
+          url: "http://8.142.9.0:8080/geoserver/WebGisdl/wms",
+          crossOrigin: "anonymous",
+          params: {
+            LAYERS: "WebGisdl:t2019_511802_dltb",
+            VERSION: "1.1.0",
+          },
+          projection: "EPSG:4522",
+        }),
+      });
+
+      // 4、叠加图层坐标系 21781
+      layers["wms21781"] = new TileLayer({
+        source: new TileWMS({
+          crossOrigin: "anonymous",
+          params: {
+            LAYERS: "ch.swisstopo.pixelkarte-farbe-pk1000.noscale",
+            FORMAT: "image/jpeg",
+          },
+          url: "https://wms.geo.admin.ch/",
+          projection: "EPSG:21781",
+        }),
+      });
+
+      //  5、在3857的视图空间里加载不同坐标系的图层
+      new Map({
+        layers: [layers["wms4522"]],
+        target: "map",
+        view: new View({
+          projection: "EPSG:4326",
+          center: [0, 0],
+          zoom: 2,
+        }),
+      });
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,8 +102,13 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  width: 100%;
 }
-
+#map {
+  height: 100%;
+  width: 100%;
+}
 #nav {
   padding: 30px;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,170 @@
+<!--
+ * @Author: heht
+ * @Date: 2022-03-31 21:21:25
+ * @LastEditors: heht
+ * @LastEditTime: 2022-07-15 17:08:37
+ * @Description:图片预加载
+-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="map" class="mapCon"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import "ol/ol.css";
+import Map from "ol/Map";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
+import { TileArcGISRest } from "ol/source";
+import TileGrid from "ol/tilegrid/TileGrid";
+import { ImageArcGISRest } from "ol/source";
+import { Image as ImageLayer } from "ol/layer";
+import TileImage from "ol/source/TileImage";
+import * as size from "ol/size";
+import qs from "qs";
+
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      jpgUrl: require("@/assets/pic-jpg.jpg"),
+      pngUrl: require("@/assets/pic-png.png"),
+      svgUrl: require("@/assets/pic-svg.svg"),
+      webpUrl: require("@/assets/pic-webp.webp"),
+      resolutions: [
+        0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
+        0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125,
+        0.001373291015625, 0.0006866455078125, 0.00034332275390625,
+        0.000171661376953125, 0.0000858306884765625, 0.00004291534423828125,
+        0.000021457672119140625, 0.000010728836059570312,
+        0.000005364418029785156, 0.000002682209014892578,
+        0.000001341104507446289,
+      ],
+    };
+  },
+
+  mounted() {
+    this.mapInit();
+    // this.preLoadImg();
+  },
+
+  methods: {
+    mapInit() {
+      proj4.defs("EPSG:4490", "+proj=longlat +ellps=GRS80 +no_defs");
+      register(proj4);
+      const map = new Map({
+        target: "map",
+        view: new View({
+          center: [112.39, 30],
+          zoom: 6,
+          // minZoom: 6,
+          // maxZoom: 6,
+          projection: "EPSG:4490",
+          resolutions: this.resolutions,
+        }),
+      });
+      const tileLayer = this.addArcGISTileLayer({
+        url: "http://58.246.138.178:8943/arcgis/rest/services/YZT/YX2016/MapServer",
+      });
+
+      const dynamicLayer = this.addArcGISImageLayer({
+        url: "http://58.246.138.178:8943/arcgis/rest/services/YZT/YZTYLSJ/MapServer",
+        layerIDs: [31],
+      });
+      map.addLayer(tileLayer);
+      map.addLayer(dynamicLayer);
+    },
+
+    addArcGISTileLayer({ url }) {
+      return new TileLayer({
+        source: new TileArcGISRest({
+          url: url,
+        }),
+      });
+    },
+
+    addArcGISImageLayer({ url, layerIDs }) {
+      return new ImageLayer({
+        source: new ImageArcGISRest({
+          ratio: 1,
+          params: {
+            LAYERS: `show:${layerIDs.join(",")}`,
+          },
+          url: url,
+        }),
+      });
+    },
+
+    createArcGISDynamicSource(layerInfo, eyemap) {
+      const tileGrid = new TileGrid({
+        resolutions: eyemap.getView().getResolutions(),
+        origin: [0, 0],
+      });
+
+      const source = new TileImage({
+        tileGrid,
+        projection: eyemap.getView().getProjection(),
+        crossOrigin: "anonymous",
+      });
+
+      source.setTileUrlFunction(function (tileCoord, pixelRatio) {
+        if (tileGrid.getResolutions().length <= tileCoord[0]) {
+          return undefined;
+        }
+        const tileExtent = tileGrid.getTileCoordExtent(tileCoord);
+
+        let tileSize = size.toSize(
+          tileGrid.getTileSize(tileCoord[0]),
+          this.tmpSize
+        );
+        if (pixelRatio !== 1) {
+          tileSize = size.scale(tileSize, pixelRatio, this.tmpSize);
+        }
+        const params = {
+          F: "image",
+          FORMAT: "PNG32",
+          TRANSPARENT: true,
+          SIZE: tileSize.join(","),
+          BBOX: tileExtent.join(","),
+          BBOXSR: 4490,
+          IMAGESR: 4490,
+          DPI: 96,
+        };
+        return `${layerInfo.url}/export?${qs.stringify(params)}`;
+      });
+
+      // source.updateParams = function (params = {}) {
+      //   this.params_ = {
+      //     ...(this.params_ || {}),
+      //     ...params,
+      //   };
+      // };
+      return source;
+    },
+
+    preLoadImg() {
+      const imgArr = new Array();
+      for (let i = 0; i < 100; i++) {
+        imgArr[i] = new Image(800, 400);
+        imgArr[i].src = this.jpgUrl;
+        const imgCon = document.getElementById("img-container");
+        imgCon.appendChild(imgArr[i]);
+      }
+    },
   },
 };
 </script>
+
+<style scoped>
+.mapCon {
+  width: 100%;
+  height: 100%;
+}
+.img {
+  width: 100%;
+  height: 600px;
+}
+</style>
